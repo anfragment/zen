234eaf1cabca1d60a1346dc016a546d318ffdbf4
Ansar Smagulov <ansar@anfragment.net>
2023-06-09 00:45:51 +0600

    wip: matching logic

--- filter/filter.go
+++ filter/filter.go
@@ -1,104 +1,28 @@
 package filter
 
-import (
-	"bufio"
-	"fmt"
-	"log"
-	"net/http"
-	"regexp"
-	"sync"
-	"time"
-)
-
 type Filter struct {
-	// hosts is a map of hostnames we want to filter.
-	hosts   map[string]struct{}
-	hostsMu sync.RWMutex
 }
 
 func NewFilter() *Filter {
-	return &Filter{
-		hosts: make(map[string]struct{}),
-	}
+	return &Filter{}
 }
-
-var (
-	// domainCG is a capture group for a domain name.
-	domainCG            = `((?:[\da-z][\da-z_-]*\.)+[\da-z-]*[a-z])`
-	reHosts             = regexp.MustCompile(fmt.Sprintf(`^(?:0\.0\.0\.0|127\.0\.0\.1) %s`, domainCG))
-	reHostsDomainIgnore = regexp.MustCompile(`^(?:0\.0\.0\.0|broadcasthost|local|localhost(?:\.localdomain)?|ip6-\w+)$`)
-	reFilterDomain      = regexp.MustCompile(fmt.Sprintf(`^\|\|%s\^$`, domainCG))
-)
 
 // AddRemoteFilters parses the hosts files at the given URLs and adds the hosts
 // listed in them to the filter.
 func (f *Filter) AddRemoteFilters(urls []string) error {
-	var wg sync.WaitGroup
-
-	errCount := 0
-	for _, url := range urls {
-		wg.Add(1)
-		go func(url string) {
-			defer wg.Done()
-			if err := f.AddRemoteFilter(url); err != nil {
-				errCount++
-				log.Printf("error adding remote hosts: %v", err)
-			}
-		}(url)
-	}
-
-	wg.Wait()
-	if errCount == len(urls) {
-		return fmt.Errorf("error adding all remote hosts")
-	}
 	return nil
 }
 
 // AddRemoteFilter parses the hosts file at the given URL and adds the hosts
 // listed in it to the filter.
 func (f *Filter) AddRemoteFilter(url string) error {
-	start := time.Now()
-
-	resp, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	scanner := bufio.NewScanner(resp.Body)
-	c := 0
-	for scanner.Scan() {
-		if m := reHosts.FindStringSubmatch(scanner.Text()); m != nil {
-			host := m[1]
-			if reHostsDomainIgnore.MatchString(host) {
-				continue
-			}
-			f.addHost(host)
-			c++
-			continue
-		}
-		if m := reFilterDomain.FindStringSubmatch(scanner.Text()); m != nil {
-			host := m[1]
-			f.addHost(host)
-			c++
-			continue
-		}
-	}
-
-	log.Printf("Added %d hosts from %s in %s", c, url, time.Since(start))
 	return nil
 }
 
 func (f *Filter) addHost(host string) {
-	f.hostsMu.Lock()
-	defer f.hostsMu.Unlock()
-	f.hosts[host] = struct{}{}
 }
 
 // IsBlocked returns true if the given host is blocked by the filter.
 func (f *Filter) IsBlocked(host string) bool {
-	f.hostsMu.RLock()
-	defer f.hostsMu.RUnlock()
-	_, ok := f.hosts[host]
-	return ok
+	return false
 }
--- filter/matcher.go
+++ filter/matcher.go
@@ -1,88 +1,178 @@
 package filter
 
 import (
+	"fmt"
 	"regexp"
 	"sync"
 )
 
-type matcherKey struct {
-	// start is true if this node should only match the start of a string.
-	start bool
-	// token is the token to match.
+// nodeKind is the type of a node in the trie.
+type nodeKind int
+
+const (
+	nodeKindExactMatch   nodeKind = iota
+	nodeKindAddressRoot           // |
+	nodeKindHostnameRoot          // hosts.txt
+	nodeKindDomain                // ||
+	nodeKindWildcard              // *
+	nodeKindSeparator             // ^
+)
+
+// nodeKey identifies a node in the trie.
+// It is a combination of the node kind and the token that the node represents.
+// The token is only present for nodes with kind nodeKindExactMatch.
+// The other kinds of nodes only represent roots of subtrees.
+type nodeKey struct {
+	kind  nodeKind
 	token string
 }
 
-type matcherNode struct {
-	children   map[matcherKey]*matcherNode
+// node is a node in the trie.
+type node struct {
+	children   map[nodeKey]*node
 	childrenMu sync.RWMutex
-	leaf       bool
+	// isLeaf is true if the node is a leaf node and should
+	// terminate the traversal in case of a match.
+	isLeaf bool
 }
 
+func (n *node) findOrAddChild(key nodeKey) *node {
+	n.childrenMu.RLock()
+	child, ok := n.children[key]
+	n.childrenMu.RUnlock()
+	if ok {
+		return child
+	}
+
+	n.childrenMu.Lock()
+	child = &node{
+		children: make(map[nodeKey]*node),
+	}
+	n.children[key] = child
+	n.childrenMu.Unlock()
+	return child
+}
+
+func (n *node) findChild(key nodeKey) *node {
+	n.childrenMu.RLock()
+	child := n.children[key]
+	n.childrenMu.RUnlock()
+	return child
+}
+
+// match returns true if the node's subtree matches the given tokens.
+func (n *node) match(tokens []string) bool {
+	if n == nil || len(tokens) == 0 {
+		return false
+	}
+	if n.isLeaf {
+		return true
+	}
+
+	return n.findChild(nodeKey{kind: nodeKindExactMatch, token: tokens[0]}).match(tokens[1:])
+}
+
+// Matcher is trie-based matcher for URLs that is capable of parsing
+// Adblock filter and hosts rules and matching URLs against them.
+//
+// The matcher is safe for concurrent use.
 type Matcher struct {
-	root *matcherNode
+	root *node
 }
 
 func NewMatcher() *Matcher {
 	return &Matcher{
-		root: &matcherNode{
-			children: make(map[matcherKey]*matcherNode),
+		root: &node{
+			children: make(map[nodeKey]*node),
 		},
 	}
 }
 
+var (
+	// hostnameCG is a capture group for a hostname.
+	hostnameCG    = `((?:[\da-z][\da-z_-]*\.)+[\da-z-]*[a-z])`
+	reHosts       = regexp.MustCompile(fmt.Sprintf(`^(?:0\.0\.0\.0|127\.0\.0\.1) %s`, hostnameCG))
+	reHostsIgnore = regexp.MustCompile(`^(?:0\.0\.0\.0|broadcasthost|local|localhost(?:\.localdomain)?|ip6-\w+)$`)
+)
+
 func (m *Matcher) AddRule(rule string) {
-	tokens := tokenize(rule)
-	node := m.root
-	for i, token := range tokens {
-		key := matcherKey{
-			start: false,
-			token: token,
+	rootKeyKind := nodeKindExactMatch
+	var tokens []string
+
+	if host := reHosts.FindStringSubmatch(rule); host != nil {
+		if !reHostsIgnore.MatchString(host[1]) {
+			rootKeyKind = nodeKindHostnameRoot
+			tokens = tokenize(host[1])
 		}
-		node.childrenMu.RLock()
-		child, ok := node.children[key]
-		node.childrenMu.RUnlock()
-		if ok {
-			node = child
-			if i == len(tokens)-1 {
-				node.leaf = true
-			}
+	}
+
+	// temporary
+	if len(tokens) == 0 {
+		return
+	}
+
+	node := m.root.findOrAddChild(nodeKey{kind: rootKeyKind})
+	for i, token := range tokens {
+		// TODO: handle separators, wildcards, etc.
+		if i == len(tokens)-1 {
+			node.findOrAddChild(nodeKey{kind: nodeKindExactMatch, token: token}).isLeaf = true
 		} else {
-			child = &matcherNode{
-				leaf: i == len(tokens)-1,
-			}
-			node.childrenMu.Lock()
-			if node.children == nil {
-				node.children = make(map[matcherKey]*matcherNode, 1)
-			}
-			node.children[key] = child
-			node.childrenMu.Unlock()
-			node.leaf = false
-			node = child
+			node = node.findOrAddChild(nodeKey{kind: nodeKindExactMatch, token: token})
 		}
 	}
 }
 
+// Match returns true if the given URL matches any of the rules.
+// It expects the URL to be in the fully qualified form.
 func (m *Matcher) Match(url string) bool {
+	// address root -> hostname root -> domain -> etc.
 	tokens := tokenize(url)
-	for i := range tokens {
-		node := m.root
-		for j := i; j < len(tokens); j++ {
-			key := matcherKey{
-				start: false,
-				token: tokens[j],
-			}
-			node.childrenMu.RLock()
-			child, ok := node.children[key]
-			node.childrenMu.RUnlock()
-			if !ok {
-				break
-			}
-			node = child
-			if node.leaf {
+
+	// address root
+	if match := m.root.findChild(nodeKey{kind: nodeKindAddressRoot}).match(tokens); match {
+		return true
+	}
+	if match := m.root.match(tokens); match {
+		return true
+	}
+	tokens = tokens[1:]
+
+	// protocol separator
+	if match := m.root.match(tokens); match {
+		return true
+	}
+	tokens = tokens[1:]
+
+	// hostname root
+	if match := m.root.findChild(nodeKey{kind: nodeKindHostnameRoot}).match(tokens); match {
+		return true
+	}
+
+	// domain segments
+	for len(tokens) > 0 {
+		if tokens[0] == "/" {
+			break
+		}
+		if tokens[0] != "." {
+			if match := m.root.findChild(nodeKey{kind: nodeKindDomain}).match(tokens); match {
 				return true
 			}
 		}
+		if match := m.root.match(tokens); match {
+			return true
+		}
+		tokens = tokens[1:]
 	}
+
+	// rest of the URL
+	// TODO: handle query parameters, etc.
+	for len(tokens) > 0 {
+		if match := m.root.match(tokens); match {
+			return true
+		}
+		tokens = tokens[1:]
+	}
+
 	return false
 }
 
