fe8191dca2bdc5b2a4a6fb20165eac7c2ca3e8eb
Ansar Smagulov <ansar@anfragment.net>
2023-08-12 00:07:47 +0600

    wip(certmanager): linux installation

---
+++ certmanager/install_linux.go
@@ -0,0 +1,47 @@
+package certmanager
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func (cm *CertManager) install() error {
+	var systemTrustCommand []string
+	var systemTrustPath string
+	if pathExists("/etc/pki/ca-trust/source/anchors/") {
+		systemTrustPath = "/etc/pki/ca-trust/source/anchors/%s.pem"
+		systemTrustCommand = []string{"update-ca-trust", "extract"}
+	} else if pathExists("/usr/local/share/ca-certificates/") {
+		systemTrustPath = "/usr/local/share/ca-certificates/%s.crt"
+		systemTrustCommand = []string{"update-ca-certificates"}
+	} else if pathExists("/etc/ca-certificates/trust-source/anchors/") {
+		systemTrustPath = "/etc/ca-certificates/trust-source/anchors/%s.crt"
+		systemTrustCommand = []string{"trust", "extract-compat"}
+	} else if pathExists("/usr/share/pki/trust/anchors") {
+		systemTrustPath = "/usr/share/pki/trust/anchors/%s.pem"
+		systemTrustCommand = []string{"update-ca-certificates"}
+	}
+
+	filename := fmt.Sprintf(systemTrustPath, "zen-rootCA")
+	cmd := exec.Command("sudo", "tee", filename)
+	cmd.Stdin = bytes.NewReader(cm.CertData)
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("failed to install CA: %v\n%s", err, out)
+	}
+
+	cmd = exec.Command(systemTrustCommand...)
+	out, err = cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("failed to install CA: %v\n%s", err, out)
+	}
+
+	return nil
+}
+
+func pathExists(path string) bool {
+	_, err := os.Stat(path)
+	return err == nil
+}
