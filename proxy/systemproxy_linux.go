5919e1cfca69ba6e0fe2972d128248651419a9bb
Ansar Smagulov <ansar@anfragment.net>
2023-08-21 22:40:12 +0600

    feat: automatic proxy setup on Linux

---
+++ proxy/systemproxy_linux.go
@@ -0,0 +1,47 @@
+package proxy
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+func (p *Proxy) setSystemProxy() error {
+	if binaryExists("gsettings") {
+		commands := [][]string{
+			[]string{"gsettings", "set", "org.gnome.system.proxy", "mode", "manual"},
+			[]string{"gsettings", "set", "org.gnome.system.proxy.http", "host", p.host},
+			[]string{"gsettings", "set", "org.gnome.system.proxy.http", "port", fmt.Sprint(p.port)},
+			[]string{"gsettings", "set", "org.gnome.system.proxy.https", "host", p.host},
+			[]string{"gsettings", "set", "org.gnome.system.proxy.https", "port", fmt.Sprint(p.port)}
+		}
+
+		for _, command := range commands {
+			cmd := exec.Command(command[0], command[1:]...)
+			out, err := cmd.CombinedOutput()
+			if err != nil {
+				return fmt.Errorf("%s: %v\n%s", strings.Join(command, " "), err, out)
+			}
+		}
+	}
+	// Proxy configuration on non-GNOME systems is finicky
+	// and I don't have the expertise to figure it out.
+	// TODO: add support for other environments
+
+	return fmt.Errorf("system proxy configuration is currently only supported on gnome")
+}
+
+func (p *Proxy) unsetSystemProxy() error {
+	command := []string{"gsettings", "set", "org.gnome.system.proxy", "mode", "none"}
+	cmd := exec.Command(command[0], command[1:]...)
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("%s: %v\n%s", strings.Join(command, " "), err, out)
+	}
+
+	return nil
+}
+
+func binaryExists(name string) bool {
+	_, err := exec.LookPath(name)
+	return err == nil
+}
