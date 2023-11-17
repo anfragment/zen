c8111ea31b5140ce9b28e9b9b3021f4da49c02a9
Ansar Smagulov <ansar@anfragment.net>
2023-11-17 22:44:58 +0600

    perf: optimize memory usage by storing small amounts of children in an array

--- filter/ruletree/node.go
+++ filter/ruletree/node.go
@@ -8,7 +8,7 @@ import (
 	"github.com/anfragment/zen/filter/ruletree/rule"
 )
 
-// nodeKind is the type of a node in the trie.
+// nodeKind defines the type of a node in the trie.
 type nodeKind int8
 
 const (
@@ -19,60 +19,96 @@ const (
 	nodeKindSeparator            // ^
 )
 
-// nodeKey identifies a node in the trie.
-// It is a combination of the node kind and the token that the node represents.
-// The token is only present for nodes with kind nodeKindExactMatch.
-// Other kinds of nodes only represent roots of subtrees.
+// nodeKey uniquely identifies a node within the trie.
+// It comprises the node's kind and the token that the node represents.
+// The token is included only for nodes of the type 'nodeKindExactMatch'.
+// Nodes of other kinds represent the roots of subtrees without including a token.
 type nodeKey struct {
 	kind  nodeKind
 	token string
 }
 
-// node is a node in the trie.
+// arrNode is a node in the trie that is stored in an array.
+type arrNode struct {
+	key  nodeKey
+	node *node
+}
+
+// nodeChildrenMaxArrSize specifies the maximum size for the array of child nodes.
+// When the array's size exceeds this value, it is converted into a map.
+// This aims to optimize memory usage since most nodes have only a few children.
+// In Go, an empty map occupies 48 bytes of memory on 64-bit systems.
+// See: https://go.dev/src/runtime/map.go
+const nodeChildrenMaxArrSize = 8
+
+// node represents a node in the rule trie.
+// Nodes can be both vertices that only represent a subtree and leaves that represent a rule.
 type node struct {
-	children   map[nodeKey]*node
-	childrenMu sync.RWMutex
+	childrenArr []arrNode
+	childrenMap map[nodeKey]*node
+	childrenMu  sync.RWMutex
 	// rules is the list of rules that match the node's subtree.
-	// If it is empty, the node is not a leaf.
+	// The list is empty for nodes that do not represent a rule.
 	rules []rule.Rule
 }
 
+// findOrAddChild finds or adds a child node with the given key.
 func (n *node) findOrAddChild(key nodeKey) *node {
-	n.childrenMu.RLock()
-	child, ok := n.children[key]
-	n.childrenMu.RUnlock()
-	if ok {
+	n.childrenMu.Lock()
+	defer n.childrenMu.Unlock()
+
+	if n.childrenMap == nil {
+		for _, arrNode := range n.childrenArr {
+			if arrNode.key == key {
+				return arrNode.node
+			}
+		}
+		if len(n.childrenArr) < nodeChildrenMaxArrSize {
+			newNode := &node{}
+			n.childrenArr = append(n.childrenArr, arrNode{key: key, node: newNode})
+			return newNode
+		}
+		n.childrenMap = make(map[nodeKey]*node)
+		for _, arrNode := range n.childrenArr {
+			n.childrenMap[arrNode.key] = arrNode.node
+		}
+		n.childrenArr = nil
+	}
+
+	if child, ok := n.childrenMap[key]; ok {
 		return child
 	}
 
-	n.childrenMu.Lock()
-	child = &node{}
-	if n.children == nil {
-		n.children = make(map[nodeKey]*node)
-	}
-	n.children[key] = child
-	n.childrenMu.Unlock()
-	return child
+	newNode := &node{}
+	n.childrenMap[key] = newNode
+	return newNode
 }
 
+// FindChild returns the child node with the given key.
 func (n *node) FindChild(key nodeKey) *node {
 	n.childrenMu.RLock()
-	child := n.children[key]
-	n.childrenMu.RUnlock()
-	return child
+	defer n.childrenMu.RUnlock()
+
+	if n.childrenMap == nil {
+		for _, arrNode := range n.childrenArr {
+			if arrNode.key == key {
+				return arrNode.node
+			}
+		}
+		return nil
+	}
+	return n.childrenMap[key]
 }
 
 var (
 	// reSeparator is a regular expression that matches the separator token.
-	// according to the https://adguard.com/kb/general/ad-filtering/create-own-filters/#basic-rules-special-characters
+	// According to https://adguard.com/kb/general/ad-filtering/create-own-filters/#basic-rules-special-characters:
 	// "Separator character is any character, but a letter, a digit, or one of the following: _ - . %. ... The end of the address is also accepted as separator."
 	reSeparator = regexp.MustCompile(`[^a-zA-Z0-9]|[_\-.%]`)
 )
 
-// Match returns true if the node's subtree matches the given tokens.
-//
-// If a matching rule is found, it is returned along with the remaining tokens.
-// If no matching rule is found, nil is returned.
+// Match returns the node that matches the given tokens along with the remaining tokens.
+// If no node matches, it returns nil.
 func (n *node) Match(tokens []string) (*node, []string) {
 	if n == nil {
 		return nil, nil
@@ -100,6 +136,7 @@ func (n *node) Match(tokens []string) (*node, []string) {
 	return n.FindChild(nodeKey{kind: nodeKindExactMatch, token: tokens[0]}).Match(tokens[1:])
 }
 
+// TraverseAndHandleReq traverses the trie and returns the action to take for the given request.
 func (n *node) TraverseAndHandleReq(req *http.Request, tokens []string, shouldUseNode func(*node, []string) bool) rule.RequestAction {
 	if n == nil {
 		return rule.RequestAction{Type: rule.ActionAllow}
@@ -142,6 +179,7 @@ func (n *node) TraverseAndHandleReq(req *http.Request, tokens []string, shouldUs
 	return n.FindChild(nodeKey{kind: nodeKindExactMatch, token: tokens[0]}).TraverseAndHandleReq(req, tokens[1:], shouldUseNode)
 }
 
+// HandleRequest returns the action to take for the given request.
 func (n *node) HandleRequest(req *http.Request) rule.RequestAction {
 	for _, r := range n.rules {
 		action := r.HandleRequest(req)
