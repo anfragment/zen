0fdd8fb0ed00336324e865bb4c963e56b69c4223
Ansar Smagulov <ansar@anfragment.net>
2023-12-04 22:28:28 +0600

    feat(frontend): proper country flag emojis on all platforms

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,23 @@
+import { isEmojiSupported } from 'is-emoji-supported';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 import './style.css';
 import App from './App';
+
+(function polyfillCountryFlagEmojis() {
+  if (!isEmojiSupported('😊') || isEmojiSupported('🇨🇭')) {
+    return;
+  }
+
+  const style = document.createElement('style');
+  style.innerHTML = `
+      body, html {
+        font-family: 'Twemoji Country Flags', Inter, Roboto, 'Helvetica Neue', 'Arial Nova', 'Nimbus Sans', Arial, sans-serif;
+      }
+    `;
+  document.head.appendChild(style);
+})();
 
 const container = document.getElementById('root');
 
