67ea9ff3ad03500cabaf594cb566fb213325f80f
Ansar Smagulov <ansar@anfragment.net>
2023-12-28 13:29:05 +0600

    fix: panic on parsing all modifier

--- filter/ruletree/rule/rule.go
+++ filter/ruletree/rule/rule.go
@@ -60,6 +60,7 @@ func (rm *Rule) ParseModifiers(modifiers string) error {
 			modifier = &removeParamModifier{}
 		case isKind("all"):
 			// TODO: should act as "popup" modifier once it gets implemented
+			continue
 		default:
 			return fmt.Errorf("unknown modifier %s", modifier)
 		}
