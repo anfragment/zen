d7241ad17fe4c5354e7c0b58c808d4e8e0af2d58
Ansar Smagulov <ansar@anfragment.net>
2024-02-06 16:49:28 +0600

    test(filter): additional domain modifier tests

--- filter/ruletree/rule/domain_test.go
+++ filter/ruletree/rule/domain_test.go
@@ -6,19 +6,26 @@ import (
 	"testing"
 )
 
-func TestSingleDomai(t *testing.T) {
+func TestSingleDomain(t *testing.T) {
 	t.Parallel()
 
 	m := domainModifier{}
-	m.Parse("domain=example.com")
+	if err := m.Parse("domain=example.com"); err != nil {
+		t.Fatal(err)
+	}
+
 	req := http.Request{
 		Header: http.Header{
 			"Referer": []string{"http://example.com/"},
 		},
 	}
-
 	if !m.ShouldMatch(&req) {
 		t.Error("domain=example.com should match a request with example.com as the referer")
+	}
+
+	req.Header.Set("Referer", "http://sub.example.com/path")
+	if !m.ShouldMatch(&req) {
+		t.Error("domain=example.com should match a request with sub.example.com as the referer")
 	}
 
 	req.Header.Set("Referer", "http://example.org/")
@@ -31,13 +38,15 @@ func TestSingleInvertedDomain(t *testing.T) {
 	t.Parallel()
 
 	m := domainModifier{}
-	m.Parse("domain=~example.com")
+	if err := m.Parse("domain=~example.com"); err != nil {
+		t.Fatal(err)
+	}
+
 	req := http.Request{
 		Header: http.Header{
 			"Referer": []string{"http://test.com/"},
 		},
 	}
-
 	if !m.ShouldMatch(&req) {
 		t.Error("domain=~example.com should match a request with test.com as the referer")
 	}
@@ -46,13 +55,75 @@ func TestSingleInvertedDomain(t *testing.T) {
 	if m.ShouldMatch(&req) {
 		t.Error("domain=~example.com should not match a request with example.com as the referer")
 	}
+
+	req.Header.Set("Referer", "http://sub.example.com/path")
+	if m.ShouldMatch(&req) {
+		t.Error("domain=~example.com should not match a request with sub.example.com as the referer")
+	}
+}
+
+func TestTLD(t *testing.T) {
+	t.Parallel()
+
+	m := domainModifier{}
+	if err := m.Parse("domain=example.*"); err != nil {
+		t.Fatal(err)
+	}
+
+	req := http.Request{
+		Header: http.Header{
+			"Referer": []string{"http://example.com/"},
+		},
+	}
+	if !m.ShouldMatch(&req) {
+		t.Error("domain=example.* should match a request with example.com as the referer")
+	}
+
+	req.Header.Set("Referer", "https://example.co.uk/some/path")
+	if !m.ShouldMatch(&req) {
+		t.Error("domain=example.* should match a request with example.co.uk as the referer")
+	}
+
+	req.Header.Set("Referer", "http://test.com")
+	if m.ShouldMatch(&req) {
+		t.Error("domain=example.* should not match a request with test.com as the referer")
+	}
+}
+
+func TestRegex(t *testing.T) {
+	t.Parallel()
+
+	m := domainModifier{}
+	if err := m.Parse(`domain=/^example\.(com|org)$/`); err != nil {
+		t.Fatal(err)
+	}
+	req := http.Request{
+		Header: http.Header{
+			"Referer": []string{"http://example.com/"},
+		},
+	}
+	if !m.ShouldMatch(&req) {
+		t.Error(`domain=/^example\.(com|org)$/ should match a request with example.com as the referer`)
+	}
+
+	req.Header.Set("Referer", "http://example.org/")
+	if !m.ShouldMatch(&req) {
+		t.Error(`domain=/^example\.(com|org)$/ should match a request with example.org as the referer`)
+	}
+
+	req.Header.Set("Referer", "http://example.net/")
+	if m.ShouldMatch(&req) {
+		t.Error(`domain=/^example\.(com|org)$/ should not match a request with example.net as the referer`)
+	}
 }
 
 func TestMultipleDomains(t *testing.T) {
 	t.Parallel()
 
 	m := domainModifier{}
-	m.Parse("domain=example.com|example.org")
+	if err := m.Parse("domain=example.com|example.org"); err != nil {
+		t.Fatal(err)
+	}
 
 	req := http.Request{
 		Header: http.Header{
@@ -78,7 +149,9 @@ func TestMultipleInvertedDomains(t *testing.T) {
 	t.Parallel()
 
 	m := domainModifier{}
-	m.Parse("domain=~example.com|~example.org")
+	if err := m.Parse("domain=~example.com|~example.org"); err != nil {
+		t.Fatal(err)
+	}
 
 	req := http.Request{
 		Header: http.Header{
@@ -100,11 +173,13 @@ func TestMultipleInvertedDomains(t *testing.T) {
 	}
 }
 
-func TestHostnameMatching(t *testing.T) {
+func TestURLMatching(t *testing.T) {
 	t.Parallel()
 
 	m := domainModifier{}
-	m.Parse("domain=example.com")
+	if err := m.Parse("domain=example.com"); err != nil {
+		t.Fatal(err)
+	}
 
 	url, _ := url.Parse("http://example.com/")
 	req := http.Request{
@@ -123,11 +198,14 @@ func TestHostnameMatching(t *testing.T) {
 	}
 }
 
-func TestHostnameMatchingWithReferer(t *testing.T) {
+// TestURLMatchingWithReferer tests whether the Referer header takes precedence over the URL.
+func TestURLMatchingWithReferer(t *testing.T) {
 	t.Parallel()
 
 	m := domainModifier{}
-	m.Parse("domain=example.com")
+	if err := m.Parse("domain=example.com"); err != nil {
+		t.Fatal(err)
+	}
 
 	url, _ := url.Parse("http://example.com/")
 	req := http.Request{
