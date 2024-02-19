7869f27441319249e48ba8662923d95b0238e2e6
Ansar Smagulov <ansar@anfragment.net>
2024-02-20 00:22:59 +0600

    docs: redundant comments

--- proxy/websocket.go
+++ proxy/websocket.go
@@ -1,36 +1,3 @@
-/*
- * This file contains some code originally licensed under the BSD-3-Clause license:
- * Copyright (c) 2012 Elazar Leibovich. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met:
- *
- *    * Redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer.
- *    * Redistributions in binary form must reproduce the above
- * copyright notice, this list of conditions and the following disclaimer
- * in the documentation and/or other materials provided with the
- * distribution.
- *    * Neither the name of Elazar Leibovich. nor the names of its
- * contributors may be used to endorse or promote products derived from
- * this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * The original code can be found at:
- * https://github.com/elazarl/goproxy/blob/master/websocket.go
- */
 package proxy
 
 import (
@@ -127,7 +94,7 @@ func headerContains(h http.Header, name, value string) bool {
 
 func isWS(r *http.Request) bool {
 	// RFC 6455, the WebSocket Protocol specification, does not explicitly specify if the Upgrade header
-	// should only contain the value "websocket" or not, so we'll employ some defensive programming here.
+	// should only contain the value "websocket" or not, so we employ some defensive programming here.
 	return headerContains(r.Header, "Connection", "upgrade") &&
 		headerContains(r.Header, "Upgrade", "websocket")
 }
