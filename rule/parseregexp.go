5b98a091383baabb9e2cfe261d70b7a1bcec17fe
Ansar Smagulov <ansar@anfragment.net>
2024-03-21 06:33:12 +0500

    Make a universal regexp parser for rule modifiers

---
+++ rule/parseregexp.go
@@ -0,0 +1,34 @@
+package rule
+
+import (
+	"fmt"
+	"regexp"
+)
+
+var (
+	// regexpRegexp is a regexp that matches a regexp.
+	regexpRegexp = regexp.MustCompile(`^/(.+)/i?$`)
+)
+
+// parseRegexp parses regulars expressions contained in rule modifiers.
+// It returns nil as the first value if the input doesn't look like a regular expression.
+func parseRegexp(s string) (*regexp.Regexp, error) {
+	match := regexpRegexp.FindStringSubmatch(s)
+	if match == nil {
+		return nil, nil
+	}
+
+	regexpBody := match[1]
+	if s[len(s)-1] == 'i' {
+		// Filter lists are designed for JS-based applications whose regex flag syntax is incompatible with that of Go.
+		// Therefore, we explicitly convert one to another to maintain compatibility.
+		regexpBody = "(?i)" + regexpBody
+	}
+
+	regexp, err := regexp.Compile(regexpBody)
+	if err != nil {
+		return nil, fmt.Errorf("compile regexp: %w", err)
+	}
+
+	return regexp, nil
+}
---
+++ rule/parseregexp_test.go
@@ -0,0 +1,65 @@
+package rule
+
+import "testing"
+
+func TestParseRegexp(t *testing.T) {
+	t.Parallel()
+
+	t.Run("returns nil if input is empty", func(t *testing.T) {
+		t.Parallel()
+
+		regexp, err := parseRegexp("")
+		if err != nil {
+			t.Errorf("expected error to be nil, got: %v", err)
+		}
+		if regexp != nil {
+			t.Errorf("expected regexp to be nil, got: %v", regexp)
+		}
+	})
+
+	t.Run("returns nil if input is not a regular expression", func(t *testing.T) {
+		t.Parallel()
+
+		regexp, err := parseRegexp("notaregexp")
+		if err != nil {
+			t.Errorf("expected error to be nil, got: %v", err)
+		}
+		if regexp != nil {
+			t.Errorf("expected regexp to be nil, got: %v", regexp)
+		}
+	})
+
+	t.Run("returns regexp if input is valid", func(t *testing.T) {
+		t.Parallel()
+
+		regexp, err := parseRegexp("/test/")
+		if err != nil {
+			t.Errorf("expected error to be nil, got: %v", err)
+		}
+		if regexp == nil {
+			t.Error("expected regexp to be non-nil")
+		}
+	})
+
+	t.Run("correctly interprets JS-style case insensitivity flag", func(t *testing.T) {
+		t.Parallel()
+
+		regexp, err := parseRegexp("/test/i")
+		if err != nil {
+			t.Errorf("expected error to be nil, got: %v", err)
+		}
+		if regexp == nil {
+			t.Errorf("expected regexp to be non-nil, got nil")
+		}
+
+		if !regexp.MatchString("test") {
+			t.Error("expected regexp to match \"test\"")
+		}
+		if !regexp.MatchString("TEST") {
+			t.Error("expected regexp to match \"TEST\"")
+		}
+		if regexp.MatchString("prod") {
+			t.Error("expected regexp not to match \"prod\"")
+		}
+	})
+}
