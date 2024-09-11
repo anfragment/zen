f5e54f77d640491a30ae2e1de2a32b541352bcd7
Ansar Smagulov <ansar@anfragment.net>
2024-09-11 22:33:36 +0500

    Add rollup build configuration for scriptlets

---
+++ scriptlets/src/index.ts
@@ -0,0 +1,5 @@
+export { preventFetch } from './prevent-fetch';
+export { preventXHR } from './prevent-xhr';
+export { nowebrtc } from './nowebrtc';
+export { setLocalStorageItem } from './set-local-storage-item';
+export { setSessionStorageItem } from './set-session-storage-item';
