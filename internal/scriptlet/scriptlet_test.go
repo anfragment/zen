5e1aa8c42c12b37162dd229b676de804851663fd
Ansar Smagulov <ansar@anfragment.net>
2024-10-22 02:27:40 +0500

    Add generic/hostname-specific scriptlet testing to public scriptlet package test suite

--- internal/scriptlet/scriptlet_test.go
+++ internal/scriptlet/scriptlet_test.go
@@ -11,17 +11,17 @@ import (
 	"golang.org/x/net/html"
 )
 
-func TestInjector(t *testing.T) {
+func TestInjectorPublic(t *testing.T) {
 	t.Parallel()
 
-	t.Run("makes an HTML-standards compliant injection", func(t *testing.T) {
+	t.Run("makes an HTML-standards compliant injection with a generic scriptlet", func(t *testing.T) {
 		i := newInjectorWithTrieStore(t)
 		err := i.AddRule(`#%#//scriptlet('prevent-xhr', 'example.com')`)
 		if err != nil {
 			t.Fatalf("failed to add rule: %v", err)
 		}
 
-		req, err := http.NewRequest("GET", "example.com", nil)
+		req, err := http.NewRequest("GET", "http://example.com", nil)
 		if err != nil {
 			t.Fatalf("failed to create request: %v", err)
 		}
@@ -31,31 +31,79 @@ func TestInjector(t *testing.T) {
 			t.Errorf("failed to inject: %v", err)
 		}
 
-		doc, err := html.Parse(res.Body)
-		if err != nil {
-			t.Errorf("failed to parse response body after injection: %v", err)
-		}
-
-		var metScriptTag bool
-		nodeStack := []*html.Node{doc}
-		var currNode *html.Node
-		for len(nodeStack) > 0 {
-			currNode = nodeStack[len(nodeStack)-1]
-			nodeStack = nodeStack[:len(nodeStack)-1]
-			if currNode.Type == html.ElementNode && currNode.Data == "script" {
-				metScriptTag = true
-				break
-			}
-
-			for c := currNode.FirstChild; c != nil; c = c.NextSibling {
-				nodeStack = append(nodeStack, c)
-			}
-		}
-
-		if !metScriptTag {
+		if !hasScriptTag(t, res.Body) {
 			t.Error("expected response body to contain at least one <script> tag, got 0")
 		}
 	})
+
+	t.Run("makes an HTML-standards compliant injection with a hostname-specific scriptlet", func(t *testing.T) {
+		i := newInjectorWithTrieStore(t)
+		err := i.AddRule(`news.example.com#%#//scriptlet('prevent-xhr', 'example.com')`)
+		if err != nil {
+			t.Fatalf("failed to add rule: %v", err)
+		}
+
+		req, err := http.NewRequest("GET", "http://news.example.com/frontpage", nil)
+		if err != nil {
+			t.Fatalf("failed to create request: %v", err)
+		}
+		res := newBlankHttpResponse(t)
+
+		if err := i.Inject(req, res); err != nil {
+			t.Errorf("failed to inject: %v", err)
+		}
+
+		if !hasScriptTag(t, res.Body) {
+			t.Error("expected response body to contain at least one <script> tag, got 0")
+		}
+	})
+
+	t.Run("doesn't inject scriptlets into a response without a matching rule", func(t *testing.T) {
+		i := newInjectorWithTrieStore(t)
+		err := i.AddRule(`example.com#%#//scriptlet('prevent-xhr', 'example.com')`)
+		if err != nil {
+			t.Fatalf("failed to add rule: %v", err)
+		}
+
+		req, err := http.NewRequest("GET", "http://notexamplecom.com", nil)
+		if err != nil {
+			t.Fatalf("failed to create request: %v", err)
+		}
+		res := newBlankHttpResponse(t)
+
+		if err := i.Inject(req, res); err != nil {
+			t.Errorf("failed to inject: %v", err)
+		}
+
+		if hasScriptTag(t, res.Body) {
+			t.Error("expected response body to contain 0 <script> tags, got 1")
+		}
+	})
+}
+
+func hasScriptTag(t *testing.T, body io.ReadCloser) bool {
+	t.Helper()
+	doc, err := html.Parse(body)
+	if err != nil {
+		t.Errorf("failed to parse response body after injection: %v", err)
+	}
+
+	var metScriptTag bool
+	nodeStack := []*html.Node{doc}
+	var currNode *html.Node
+	for len(nodeStack) > 0 {
+		currNode = nodeStack[len(nodeStack)-1]
+		nodeStack = nodeStack[:len(nodeStack)-1]
+		if currNode.Type == html.ElementNode && currNode.Data == "script" {
+			metScriptTag = true
+			break
+		}
+
+		for c := currNode.FirstChild; c != nil; c = c.NextSibling {
+			nodeStack = append(nodeStack, c)
+		}
+	}
+	return metScriptTag
 }
 
 func newBlankHttpResponse(t *testing.T) *http.Response {
