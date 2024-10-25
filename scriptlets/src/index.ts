e378c4b872fb0cd6315aebd8a142cbcd7b728a09
Ansar Smagulov <ansar@anfragment.net>
2024-10-25 21:12:44 +0500

    WIP json-prune scriptlet

--- scriptlets/src/index.ts
+++ scriptlets/src/index.ts
@@ -4,3 +4,4 @@ export { nowebrtc } from './nowebrtc';
 export { setLocalStorageItem } from './set-local-storage-item';
 export { setSessionStorageItem } from './set-session-storage-item';
 export { setConstant } from './set-constant';
+export { jsonPrune } from './json-prune';
