60e5cea5fd66adb775abdb2884c0df8d1fdc7aaf
Ansar Smagulov <ansar@anfragment.net>
2024-11-07 22:32:14 +0500

    Add TextEncoder and TextDecoder globals to Jest environment

--- scriptlets/src/test/environment.ts
+++ scriptlets/src/test/environment.ts
@@ -2,6 +2,8 @@
  * This file is used to add fetch globals to the JSDOM environment in Jest.
  * https://github.com/jsdom/jsdom/issues/1724#issuecomment-1446858041
  */
+import { TextEncoder, TextDecoder } from 'node:util';
+
 import JSDOMEnvironment from 'jest-environment-jsdom';
 
 // https://github.com/facebook/jest/blob/v29.4.3/website/versioned_docs/version-29.4/Configuration.md#testenvironment-string
@@ -13,5 +15,7 @@ export default class FixJSDOMEnvironment extends JSDOMEnvironment {
     this.global.Headers = Headers;
     this.global.Request = Request;
     this.global.Response = Response;
+    this.global.TextEncoder = TextEncoder;
+    this.global.TextDecoder = TextDecoder as any;
   }
 }
