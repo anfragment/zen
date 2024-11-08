fa2aa1e787b4743df26c91424ae0e4fca008f313
Ansar Smagulov <ansar@anfragment.net>
2024-11-08 18:51:31 +0500

    Add tests for jsonPruneXHRResponse to validate response handling and pruning logic

--- scriptlets/src/json-prune-xhr-response.test.ts
+++ scriptlets/src/json-prune-xhr-response.test.ts
@@ -64,7 +64,7 @@ describe('json-prune-xhr-response', () => {
     });
   });
 
-  test('leaves non-JSON arraybuffer response untouched', (done) => {
+  test('leaves non-JSON arraybuffer response intact', (done) => {
     const expectedResponse = 'hey there';
 
     setupFakeSend(new TextEncoder().encode(expectedResponse), 'arraybuffer');
@@ -102,6 +102,163 @@ describe('json-prune-xhr-response', () => {
 
       const decoded = await xhr.response.text();
       expect(decoded).toBe(JSON.stringify({ a: 123 }));
+      done();
+    });
+  });
+
+  test('leaves non-JSON blob response intact', (done) => {
+    const expectedResponse = 'hey there';
+
+    setupFakeSend(new Blob([expectedResponse]), 'blob');
+
+    jsonPruneXHRResponse('hey');
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'test');
+    xhr.send();
+
+    xhr.addEventListener('readystatechange', async () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) {
+        return;
+      }
+
+      const decoded = await xhr.response.text();
+      expect(decoded).toBe(expectedResponse);
+      done();
+    });
+  });
+
+  test('prunes json response', (done) => {
+    setupFakeSend({ a: 123, b: 123 }, 'json');
+
+    jsonPruneXHRResponse('b');
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'test');
+    xhr.send();
+
+    xhr.addEventListener('readystatechange', () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) {
+        return;
+      }
+
+      expect(xhr.response).toEqual({ a: 123 });
+      done();
+    });
+  });
+
+  test("doesn't prune response on a non-matching domain name", (done) => {
+    setupFakeSend({ a: 123, b: 123 }, 'json');
+
+    jsonPruneXHRResponse('b', '', 'example.org');
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://example.net');
+    xhr.send();
+
+    xhr.addEventListener('readystatechange', () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) {
+        return;
+      }
+
+      expect(xhr.response).toEqual({ a: 123, b: 123 });
+      done();
+    });
+  });
+
+  test('prunes response on a matching domain name', (done) => {
+    setupFakeSend({ a: 123, b: 123 }, 'json');
+
+    jsonPruneXHRResponse('b', '', 'example.org');
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://example.org');
+    xhr.send();
+
+    xhr.addEventListener('readystatechange', () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) {
+        return;
+      }
+
+      expect(xhr.response).toEqual({ a: 123 });
+      done();
+    });
+  });
+
+  test('prunes deeply nested property', (done) => {
+    setupFakeSend(
+      JSON.stringify({
+        a: {
+          b: {
+            c: {
+              d: [{ remove: 123, keep: 321 }],
+            },
+          },
+        },
+      }),
+      'text',
+    );
+
+    jsonPruneXHRResponse('a.*.remove');
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'test');
+    xhr.send();
+
+    xhr.addEventListener('readystatechange', () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) {
+        return;
+      }
+
+      expect(xhr.response).toBe(
+        JSON.stringify({
+          a: {
+            b: {
+              c: {
+                d: [{ keep: 321 }],
+              },
+            },
+          },
+        }),
+      );
+      done();
+    });
+  });
+
+  test("doesn't prune on a missing required property", (done) => {
+    setupFakeSend({ a: 123, b: 123 }, 'json');
+
+    jsonPruneXHRResponse('b', 'c');
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'test');
+    xhr.send();
+
+    xhr.addEventListener('readystatechange', () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) {
+        return;
+      }
+
+      expect(xhr.response).toEqual({ a: 123, b: 123 });
+      done();
+    });
+  });
+
+  test('prunes multiple properties', (done) => {
+    setupFakeSend({ a: 123, b: 123, c: 123 }, 'json');
+
+    jsonPruneXHRResponse('a b');
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'test');
+    xhr.send();
+
+    xhr.addEventListener('readystatechange', () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) {
+        return;
+      }
+
+      expect(xhr.response).toEqual({ c: 123 });
       done();
     });
   });
