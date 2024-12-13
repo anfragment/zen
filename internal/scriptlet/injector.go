921b15c6655c0326f3f54b0fc636330dff73fdce
Ansar Smagulov <ansar@anfragment.net>
2024-12-13 19:31:38 +0500

    Refactor scriptlet injector to utilize htmlrewrite for body content replacement

--- internal/scriptlet/injector.go
+++ internal/scriptlet/injector.go
@@ -2,23 +2,15 @@ package scriptlet
 
 import (
 	"bytes"
-	"compress/flate"
-	"compress/gzip"
 	"embed"
 	"errors"
 	"fmt"
-	"io"
 	"log"
-	"mime"
 	"net/http"
 	"regexp"
-	"strconv"
-	"strings"
 
-	"github.com/andybalholm/brotli"
+	"github.com/anfragment/zen/internal/htmlrewrite"
 	"github.com/anfragment/zen/internal/logger"
-	"github.com/klauspost/compress/zstd"
-	"golang.org/x/net/html/charset"
 )
 
 var (
@@ -67,7 +59,7 @@ func NewInjector(store Store) (*Injector, error) {
 
 // Inject injects scriptlets into a given HTTP HTML response.
 //
-// In case of an error, the response body is unchanged and the caller may proceed as if the function had not been called.
+// On error, the caller may proceed as if the function had not been called.
 func (inj *Injector) Inject(req *http.Request, res *http.Response) error {
 	hostname := req.URL.Hostname()
 	scriptlets := inj.store.Get(hostname)
@@ -88,106 +80,11 @@ func (inj *Injector) Inject(req *http.Request, res *http.Response) error {
 	ruleInjection.WriteString("})();\n")
 	ruleInjection.Write(scriptClosingTag)
 
-	rawBodyBytes, err := readRawBody(res)
-	if err != nil {
-		return fmt.Errorf("read raw body: %w", err)
-	}
-
-	modifiedBody := reBody.ReplaceAllFunc(rawBodyBytes, func(match []byte) []byte {
+	htmlrewrite.ReplaceBodyContents(res, func(match []byte) []byte {
 		match = append(match, inj.bundle...)
-		match = append(match, '\n')
 		match = append(match, ruleInjection.Bytes()...)
 		return match
 	})
 
-	res.Body = io.NopCloser(bytes.NewReader(modifiedBody))
-	res.ContentLength = int64(len(modifiedBody))
-	res.Header.Set("Content-Length", strconv.Itoa(len(modifiedBody)))
-	res.Header.Del("Content-Encoding")
-	res.Header.Set("Content-Type", "text/html; charset=utf-8")
-
 	return nil
-}
-
-// readRawBody extracts a raw body from a potentially compressed and non-UTF8 encoded HTTP response.
-//
-// In case of an error, the response body is unchanged and the caller may proceed as if the function had not been called.
-func readRawBody(res *http.Response) ([]byte, error) {
-	resBytes, err := io.ReadAll(res.Body)
-	if err != nil {
-		return nil, fmt.Errorf("read body: %w", err)
-	}
-	res.Body.Close()
-
-	decompressedReader, err := decompressReader(bytes.NewReader(resBytes), res.Header.Get("Content-Encoding"))
-	if err != nil {
-		res.Body = io.NopCloser(bytes.NewReader(resBytes))
-		return nil, fmt.Errorf("create decompressed reader: %w", err)
-	}
-
-	contentType := res.Header.Get("Content-Type")
-	_, params, err := mime.ParseMediaType(contentType)
-	if err != nil {
-		res.Body = io.NopCloser(bytes.NewReader(resBytes))
-		return nil, fmt.Errorf("parse media type: %w", err)
-	}
-	decodedReader, err := decodeReader(decompressedReader, params["charset"])
-	if err != nil {
-		decompressedReader.Close()
-		res.Body = io.NopCloser(bytes.NewReader(resBytes))
-		return nil, fmt.Errorf("create decoded reader: %w", err)
-	}
-
-	originalBody, err := io.ReadAll(decodedReader)
-	decompressedReader.Close()
-	if err != nil {
-		res.Body = io.NopCloser(bytes.NewReader(resBytes))
-		return nil, fmt.Errorf("read decompressed+decoded stream: %w", err)
-	}
-	return originalBody, nil
-}
-
-// decompressReader decompresses the reader based on the provided compression algorithm.
-// It does not decompress data encoded with multiple algorithms.
-//
-// Reference: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Encoding
-func decompressReader(reader io.Reader, compressionAlg string) (io.ReadCloser, error) {
-	switch strings.ToLower(compressionAlg) {
-	case "gzip":
-		gzipReader, err := gzip.NewReader(reader)
-		if err != nil {
-			return nil, fmt.Errorf("create gzip reader: %w", err)
-		}
-		return gzipReader, nil
-	case "deflate":
-		return flate.NewReader(reader), nil
-	case "br":
-		return io.NopCloser(brotli.NewReader(reader)), nil
-	case "zstd":
-		zstdReader, err := zstd.NewReader(reader)
-		if err != nil {
-			return nil, fmt.Errorf("create zstd reader: %w", err)
-		}
-		return io.NopCloser(zstdReader), nil
-	case "":
-		return io.NopCloser(reader), nil
-	default:
-		return nil, fmt.Errorf("unsupported encoding %q", compressionAlg)
-	}
-}
-
-// decodeReader decodes the reader based on the provided character encoding.
-//
-// Reference: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Type#media-type
-func decodeReader(reader io.Reader, encoding string) (io.Reader, error) {
-	switch strings.ToLower(encoding) {
-	case "utf-8", "us-ascii", "":
-		return reader, nil
-	default:
-		encoding, _ := charset.Lookup(encoding)
-		if encoding == nil {
-			return nil, fmt.Errorf("unsupported charset %q", encoding)
-		}
-		return encoding.NewDecoder().Reader(reader), nil
-	}
 }
