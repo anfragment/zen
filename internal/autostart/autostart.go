21c0861fcb7e682ada60691ce4a87d34abdab725
AitakattaSora <momynov.sultan@gmail.com>
2025-01-03 12:44:40 +0500

    Remove symlink evaluation in windows autostart

--- internal/autostart/autostart.go
+++ internal/autostart/autostart.go
@@ -3,7 +3,6 @@ package autostart
 import (
 	"fmt"
 	"os"
-	"path/filepath"
 )
 
 // Manager manages automatic startup of the app on user login.
@@ -14,9 +13,6 @@ func getExecPath() (string, error) {
 	execPath, err := os.Executable()
 	if err != nil {
 		return "", fmt.Errorf("get executable path: %w", err)
-	}
-	if execPath, err = filepath.EvalSymlinks(execPath); err != nil {
-		return "", fmt.Errorf("eval symlinks: %w", err)
 	}
 	return execPath, nil
 }
