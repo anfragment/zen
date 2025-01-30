06118eff16b00f045e29dbbcf35a85a1c37f60a4
Ansar Smagulov <ansar@anfragment.net>
2025-01-30 21:08:00 +0500

    Implement streaming injections (#212)
    * Improve/fix hop-by-hop header management in CONNECT proxying; proactively close connections when clients' or servers' transmit the intention to do so themselves
    
    * Rewrite htmlrewrite to enable streaming injection into response without having to first read for the entire body
    
    * Implement htmlrewrite.PrependHeadContents for use in scriptlet injector
    
    * Rewrite htmlrewrite tests to be table-driven tests; replace crypto/rand with math/rand in genAlphanumByteArray

--- internal/htmlrewrite/rawbody.go
+++ internal/htmlrewrite/rawbody.go
@@ -1,59 +1,58 @@
 package htmlrewrite
 
 import (
-	"bytes"
 	"compress/flate"
 	"compress/gzip"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
 	"strings"
 
 	"github.com/andybalholm/brotli"
+	"github.com/hashicorp/go-multierror"
 	"github.com/klauspost/compress/zstd"
 	"golang.org/x/net/html/charset"
 )
 
-// readRawBody extracts an uncompressed, UTF-8 decoded body from a potentially compressed and non-UTF-8 encoded HTTP response.
-//
-// On error, the response body is unchanged and the caller may proceed as if the function had not been called.
-func readRawBody(res *http.Response) ([]byte, error) {
-	resBytes, err := io.ReadAll(res.Body)
-	if err != nil {
-		return nil, fmt.Errorf("read body: %w", err)
-	}
-	res.Body.Close()
-
-	decompressedReader, err := decompressReader(bytes.NewReader(resBytes), res.Header.Get("Content-Encoding"))
-	if err != nil {
-		res.Body = io.NopCloser(bytes.NewReader(resBytes))
-		return nil, fmt.Errorf("create decompressed reader: %w", err)
+// getRawBodyReader extracts an uncompressed, UTF-8 decoded body from a potentially compressed and non-UTF-8 encoded HTTP response.
+func getRawBodyReader(res *http.Response) (io.ReadCloser, error) {
+	encoding := res.Header.Get("Content-Encoding")
+	contentType := res.Header.Get("Content-Type")
+	if encoding == "" && strings.Contains(contentType, "charset=utf-8") {
+		// We've been here before, skip costly operations.
+		return res.Body, nil
 	}
 
-	decodedReader, err := charset.NewReader(decompressedReader, res.Header.Get("Content-Type"))
+	decompressedReader, err := decompressReader(res.Body, encoding)
+	if err != nil {
+		return nil, fmt.Errorf("create decompressed reader for encoding %q: %w", encoding, err)
+	}
+
+	decodedReader, err := charset.NewReader(decompressedReader, contentType)
 	if err != nil {
 		decompressedReader.Close()
-		res.Body = io.NopCloser(bytes.NewReader(resBytes))
-		return nil, fmt.Errorf("create decoded reader: %w", err)
+		return nil, fmt.Errorf("create decoded reader for content type %q: %w", contentType, err)
 	}
 
-	originalBody, err := io.ReadAll(decodedReader)
-	decompressedReader.Close()
-	if err != nil {
-		res.Body = io.NopCloser(bytes.NewReader(resBytes))
-		return nil, fmt.Errorf("read decompressed+decoded stream: %w", err)
-	}
-	return originalBody, nil
+	return struct {
+		io.Reader
+		io.Closer
+	}{
+		decodedReader,
+		&multiCloser{[]io.Closer{decompressedReader, res.Body}},
+	}, nil
 }
 
 // decompressReader decompresses a reader using the specified compression algorithm.
 // It does not decompress data encoded with multiple algorithms.
-func decompressReader(reader io.Reader, compressionAlg string) (io.ReadCloser, error) {
+func decompressReader(reader io.ReadCloser, compressionAlg string) (io.ReadCloser, error) {
 	// Reference: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Encoding
 	switch strings.ToLower(compressionAlg) {
 	case "gzip":
 		gzipReader, err := gzip.NewReader(reader)
 		if err != nil {
+			reader.Close()
 			return nil, fmt.Errorf("create gzip reader: %w", err)
 		}
 		return gzipReader, nil
@@ -64,12 +63,29 @@ func decompressReader(reader io.Reader, compressionAlg string) (io.ReadCloser, e
 	case "zstd":
 		zstdReader, err := zstd.NewReader(reader)
 		if err != nil {
+			zstdReader.Close()
 			return nil, fmt.Errorf("create zstd reader: %w", err)
 		}
 		return io.NopCloser(zstdReader), nil
 	case "":
-		return io.NopCloser(reader), nil
+		return reader, nil
 	default:
-		return nil, fmt.Errorf("unsupported encoding %q", compressionAlg)
+		return nil, errors.New("unsupported encoding")
 	}
+}
+
+// multiCloser wraps multiple io.Closers and ensures they are closed sequentially.
+type multiCloser struct {
+	closers []io.Closer
+}
+
+// Close iterates over each io.Closer and closes it, capturing any errors.
+func (m *multiCloser) Close() error {
+	var finalErr *multierror.Error
+	for _, closer := range m.closers {
+		if err := closer.Close(); err != nil {
+			finalErr = multierror.Append(finalErr, err)
+		}
+	}
+	return finalErr.ErrorOrNil()
 }
