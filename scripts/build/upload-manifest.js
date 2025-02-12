c432c75d04efd74edd58583a7f808b7dcdcf51c2
Ansar Smagulov <ansar@anfragment.net>
2025-02-13 03:29:06 +0500

    scripts/upload-manifest: Add arm64 asset to Linux platform manifests

--- scripts/build/upload-manifest.js
+++ scripts/build/upload-manifest.js
@@ -26,6 +26,7 @@ const PLATFORM_ASSETS = {
     },
     linux: {
         amd64: 'Zen_linux_amd64.tar.gz',
+        arm64: 'Zen_linux_arm64.tar.gz',
     },
 };
 const MANIFESTS_BASE_URL = 'https://zenprivacy.net/update-manifests/stable';
