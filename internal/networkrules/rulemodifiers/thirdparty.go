d2c3a50341fb4daec35488e7c8ef1a1c9da289b4
Ansar Smagulov <ansar@anfragment.net>
2025-03-15 19:56:30 +0500

    Rewrite $third-party matching logic to rely exclusively on the Sec-Fetch-Site header

--- internal/networkrules/rulemodifiers/thirdparty.go
+++ internal/networkrules/rulemodifiers/thirdparty.go
@@ -2,7 +2,6 @@ package rulemodifiers
 
 import (
 	"net/http"
-	"strings"
 )
 
 // https://adguard.com/kb/general/ad-filtering/create-own-filters/#third-party-modifier
@@ -20,24 +19,15 @@ func (m *ThirdPartyModifier) Parse(modifier string) error {
 }
 
 func (m *ThirdPartyModifier) ShouldMatchReq(req *http.Request) bool {
-	if req.Header.Get("Sec-Fetch-Site") == "cross-site" {
+	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/Sec-Fetch-Site
+	switch req.Header.Get("Sec-Fetch-Site") {
+	case "cross-site":
 		return !m.inverted
-	}
-
-	referer := req.Header.Get("Referer")
-	if referer == "" {
-		return false
-	}
-	targetHost := req.Host
-	refererURL, err := req.URL.Parse(referer)
-	if err != nil {
-		return false
-	}
-	refererHost := refererURL.Hostname()
-	if strings.HasSuffix(refererHost, targetHost) {
+	case "same-origin", "same-site":
 		return m.inverted
+	default:
+		return false
 	}
-	return !m.inverted
 }
 
 func (m *ThirdPartyModifier) ShouldMatchRes(_ *http.Response) bool {
