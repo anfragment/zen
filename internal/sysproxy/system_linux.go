6bb5ec8491422ddc9098ee9e8a0c5c55561d2a9e
Ansar Smagulov <ansar@anfragment.net>
2025-03-24 14:55:48 +0500

    Fix system proxy management on GNOME, refactor error messages

--- internal/sysproxy/system_linux.go
+++ internal/sysproxy/system_linux.go
@@ -11,31 +11,36 @@ var exclusionListURLs = []string{
 }
 
 func setSystemProxy(pacURL string) error {
-	if binaryExists("gsettings") {
-		commands := [][]string{
-			[]string{"gsettings", "set", "org.gnome.system.proxy", "autoconfig-url", pacURL},
-			[]string{"gsettings", "set", "org.gnome.system.proxy", "mode", "auto"},
-		}
-
-		for _, command := range commands {
-			cmd := exec.Command(command[0], command[1:]...) // #nosec G204
-			out, err := cmd.CombinedOutput()
-			if err != nil {
-				return fmt.Errorf("run system proxy command (%s): %w\n%s", strings.Join(command, " "), err, out)
-			}
-		}
-		return nil
+	if !binaryExists("gsettings") {
+		// TODO: add support for other desktop environments
+		return ErrUnsupportedDesktopEnvironment
 	}
-	// TODO: add support for other desktop environments
 
-	return ErrUnsupportedDesktopEnvironment
+	commands := [][]string{
+		[]string{"gsettings", "set", "org.gnome.system.proxy", "autoconfig-url", pacURL},
+		[]string{"gsettings", "set", "org.gnome.system.proxy", "mode", "auto"},
+	}
+
+	for _, command := range commands {
+		cmd := exec.Command(command[0], command[1:]...) // #nosec G204
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			return fmt.Errorf("run system proxy command %q: %v (%q)", strings.Join(command, " "), err, out)
+		}
+	}
+	return nil
 }
 
-func (p *Proxy) unsetSystemProxy() error {
+func unsetSystemProxy() error {
+	if !binaryExists("gsettings") {
+		// TODO: add support for other desktop environments
+		return ErrUnsupportedDesktopEnvironment
+	}
+
 	cmd := exec.Command("gsettings", "set", "org.gnome.system.proxy", "mode", "none")
 	out, err := cmd.CombinedOutput()
 	if err != nil {
-		return fmt.Errorf("failed to unset system proxy: %v\n%s", err, out)
+		return fmt.Errorf("unset system proxy: %v (%q)", err, out)
 	}
 
 	return nil
