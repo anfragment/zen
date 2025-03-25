53f2aaa186bd910466177fd79f0c0cc25dcb03de
Ansar Smagulov <ansar@anfragment.net>
2025-03-26 02:18:14 +0500

    Spread arguments in scriptlets logger for better readability

--- scriptlets/src/helpers/logger.ts
+++ scriptlets/src/helpers/logger.ts
@@ -4,19 +4,19 @@ const PRODUCT_NAME = 'Zen';
 export function createLogger(scriptletName: string) {
   return {
     log(line: string, ...context: any[]) {
-      console.log(`${PRODUCT_NAME} (${scriptletName}): ${line}`, context);
+      console.log(`${PRODUCT_NAME} (${scriptletName}): ${line}`, ...context);
     },
     debug(line: string, ...context: any[]) {
-      console.debug(`${PRODUCT_NAME} (${scriptletName}): ${line}`, context);
+      console.debug(`${PRODUCT_NAME} (${scriptletName}): ${line}`, ...context);
     },
     info(line: string, ...context: any[]) {
-      console.info(`${PRODUCT_NAME} (${scriptletName}): ${line}`, context);
+      console.info(`${PRODUCT_NAME} (${scriptletName}): ${line}`, ...context);
     },
     warn(line: string, ...context: any[]) {
-      console.warn(`${PRODUCT_NAME} (${scriptletName}): ${line}`, context);
+      console.warn(`${PRODUCT_NAME} (${scriptletName}): ${line}`, ...context);
     },
     error(line: string, ...context: any[]) {
-      console.error(`${PRODUCT_NAME} (${scriptletName}): ${line}`, context);
+      console.error(`${PRODUCT_NAME} (${scriptletName}): ${line}`, ...context);
     },
   };
 }
